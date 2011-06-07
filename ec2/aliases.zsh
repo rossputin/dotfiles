4c4ea7b5abb8a3d45c684439bd68228099f8d10e
Rossputin <rossputin@hal.(none)>
2011-06-07 18:57:25 +0100

    describe ec2 instances by my preferred region

--- ec2/aliases.zsh
+++ ec2/aliases.zsh
@@ -1,1 +1,7 @@
-alias instances="ec2-describe-instances"
+if [[ -s $HOME/.ec2/bheap_test_aws ]]
+then
+  source $HOME/.ec2/bheap_test_aws
+fi
+
+alias instances="ec2-describe-instances --region eu-west-1"
+
