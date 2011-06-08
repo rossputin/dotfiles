ac4459c062006dea5f3efb97c05e953743dfe866
Rossputin <rossputin@hal.(none)>
2011-06-08 18:16:49 +0100

    tweak ec2 aliases

--- ec2/aliases.zsh
+++ ec2/aliases.zsh
@@ -1,6 +1,6 @@
-if [[ -s $HOME/.ec2/bheap_test_aws ]]
+if [[ -s $HOME/.ec2/bheap_ec2 ]]
 then
-  source $HOME/.ec2/bheap_test_aws
+  source $HOME/.ec2/bheap_ec2
 fi
 
 alias instances="ec2-describe-instances --region eu-west-1"
