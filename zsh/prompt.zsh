a06b927d1563d5313d0b2eac6ed1b61b0f9b3d40
rossputin <ross@bheap.co.uk>
2012-08-30 09:18:14 +0100

    added prompt update for better todo and note status

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -52,10 +52,10 @@ rb_prompt(){
 # This keeps the number of todos always available the right hand side of my
 # command line. I filter it to only count those tagged as "+next", so it's more
 # of a motivation to clear out the list.
-todo(){
+todo_count(){
   if $(which todo.sh &> /dev/null)
   then
-    num=$(echo $(todo.sh ls +next | wc -l))
+    num=$(echo $(todo.sh ls $1 | wc -l))
     let todos=num-2
     if [ $todos != 0 ]
     then
@@ -68,13 +68,40 @@ todo(){
   fi
 }
 
+function todo_prompt() {
+  local COUNT=$(todo_count $1);
+  if [ $COUNT != 0 ]; then
+    echo "$1: $COUNT";
+  else
+    echo "";
+  fi
+}
+
+function notes_count() {
+  if [[ -z $1 ]]; then
+    local NOTES_PATTERN="TODO|FIXME|HACK";
+  else
+    local NOTES_PATTERN=$1;
+  fi
+  grep -ERn "\b($NOTES_PATTERN)\b" ~/Processes 2>/dev/null | wc -l | sed 's/ //g'
+}
+
+function notes_prompt() {
+  local COUNT=$(notes_count $1);
+  if [ $COUNT != 0 ]; then
+    echo "$1: $COUNT";
+  else
+    echo "";
+  fi
+}
+
 directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
 export PROMPT=$'\nrossputin in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+  export RPROMPT="%{$fg[blue]%}$(notes_prompt BHEAP)%{$reset_color%} %{$fg[green]%}$(notes_prompt APPCAVE)%{$reset_color%}  %{$fg_bold[red]%}$(todo_prompt +next)%{$reset_color%}"
 }
 
 precmd() {
