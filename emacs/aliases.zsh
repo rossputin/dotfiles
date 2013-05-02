bc1ac8b995012c377147defacc9acb3b269434de
rossputin <rossajmcd@gmail.com>
2013-05-02 20:14:07 +0100

    add emacs-live aliases

---
+++ emacs/aliases.zsh
@@ -0,0 +1,3 @@
+alias es='/Applications/Emacs.app/Contents/MacOS/Emacs --daemon'
+alias emacs='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -c -n'
+export EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -c'
